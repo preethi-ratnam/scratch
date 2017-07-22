cbd571219776337158219bf2d297b1b3f769fc5d
preethi-ratnam <30378302+preethi-ratnam@users.noreply.github.com>
2017-07-22 15:16:10 -0700

    Creating Print.java
    This prints continuous numbers as rows and cols

---
+++ Print.java
@@ -0,0 +1,11 @@
+public class Print{
+  public static void main(String[] args){
+    int rows=10, cols=5;
+    for(int i=0; i<rows; i++){
+      //System.out.print((i-1) + ((i%cols == 0) ? "\n" : " ")); 
+      for (int j=0; j<cols; j++){
+        System.out.print(i*cols+j + (j == cols-1 ? "\n" : " ")); 
+      }
+    }
+  }
+}
